d56d41b317d0352e37542cbd7e24fc3454580221
elavarasin <elavarasi1596@gmail.com>
2016-07-21 18:47:00 +0530

    df
    asd

---
+++ fact.java
@@ -0,0 +1,14 @@
+public class factorial{
+public static void main(String[] args){
+int n,c,fact=1;
+a=Integer.parseInt(args[0]);
+do
+{
+fact=n*fact;
+n++;
+}
+while(n<=1);
+{
+System.out.println("factorial  is"+i);
+}
+}
